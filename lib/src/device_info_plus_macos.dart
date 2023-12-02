0cd79ce8bb9bc06984ccef437fdd51ad9773523f
sumitsharansatsangi <sharansumitkumar@gmail.com>
2023-12-02 11:55:24 +0530

    code updated

--- lib/src/device_info_plus_macos.dart
+++ lib/src/device_info_plus_macos.dart
@@ -1,1 +1,1 @@
-/// The platform interface handles the method channel calls. This file exists to silence errors on pub.
+// The platform interface handles the method channel calls. This file exists to silence errors on pub.
