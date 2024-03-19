7dcba255515b127e359babe1806ad856db0bc0f9
sumitsharansatsangi <sharansumitkumar@gmail.com>
2024-03-19 13:04:19 +0530

    code updtaed

--- lib/src/device_info_plus_macos.dart
+++ lib/src/device_info_plus_macos.dart
@@ -1,1 +1,2 @@
-// The platform interface handles the method channel calls. This file exists to silence errors on pub.
+// ignore: dangling_library_doc_comments
+/// The platform interface handles the method channel calls. This file exists to silence errors on pub.
